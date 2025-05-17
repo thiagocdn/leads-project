9ca70d2ed08fa7bcbba6a86067a19f67f0a6f7e1
Thiago Cedran <thiagocdn@gmail.com>
2025-05-17 16:39:57 -0300

    feat: started website project

---
+++ leads-website/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center min-h-screen p-4">
+      <h1 className="text-4xl font-bold">Formulário de Contato</h1>
+      <p className="mt-4 text-lg">
+        Por favor, preencha o formulário abaixo e entraremos em contato assim
+        que possível.
+      </p>
+    </div>
+  );
+}
